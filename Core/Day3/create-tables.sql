df1f31ee23c67f00d82874bff8c74714192bc3e4
Piyush Gaur <Piyush.Gaur@poly.com>
2020-08-12 20:28:26 +0530

    create table course added

--- Core/Day3/create-tables.sql
+++ Core/Day3/create-tables.sql
@@ -9,8 +9,21 @@ CREATE TABLE Applicant
 
 CREATE TABLE Branch
 (
-  BranchId        varchar(2)   CONSTRAINT branch_pk                        PRIMARY KEY
-                                CONSTRAINT branch_bid_check                 CHECK (BranchId like 'B%'),
-  BranchName      varchar(30)  CONSTRAINT branch_name_notnull              NOT NULL
+  BranchId        varchar(2)   CONSTRAINT branch_pk             PRIMARY KEY
+    CONSTRAINT branch_bid_check                 CHECK (BranchId like 'B%'),
+  BranchName      varchar(30)  CONSTRAINT branch_name_notnull   NOT NULL
+);
+
+CREATE TABLE Course
+(
+CourseId          varchar(4)   CONSTRAINT course_pk             PRIMARY KEY,
+CourseName        varchar(30)  CONSTRAINT course_cname_notnull  NOT NULL,
+Semester          numeric(1),
+BranchId          varchar(2)   CONSTRAINT course_fk_branchid    REFERENCES Branch(BranchId),
+Elective          char(1)      CONSTRAINT course_elective_check CHECK (Elective in ('Y', 'N')),
+ProjectMarks      numeric(3),
+AssignmentMarks   numeric(3),
+InternalMarks     numeric(3),
+SemesterExamMarks numeric(3)
 );
 
