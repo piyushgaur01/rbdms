5afbdfb69eefb41b8756247991cdc4826393277a
Piyush Gaur <Piyush.Gaur@poly.com>
2020-08-12 20:14:57 +0530

    updated varchar2 to varchar to support postgres

--- Core/Day3/create-tables.sql
+++ Core/Day3/create-tables.sql
@@ -1,15 +1,16 @@
 CREATE TABLE Applicant 
 (
-  ApplicantId varchar2(4) CONSTRAINT applicant_pk PRIMARY KEY,
-  ApplicantName varchar2(30) CONSTRAINT applicant_applicantname_notnull NOT NULL,
-  Emailid varchar2(30) CONSTRAINT applicant_emailid_uq UNIQUE,
-  Address varchar2(50),
-  City varchar2(15)
+  ApplicantId     varchar(4)   CONSTRAINT applicant_pk                     PRIMARY KEY,
+  ApplicantName   varchar(30)  CONSTRAINT applicant_applicantname_notnull  NOT NULL,
+  Emailid         varchar(30)  CONSTRAINT applicant_emailid_uq UNIQUE,
+  Address         varchar(50),
+  City            varchar(15)
 );
 
 CREATE TABLE Branch
 (
-  BranchId varchar2(2) CONSTRAINT branch_pk PRIMARY KEY
-    CONSTRAINT branch_bid_check CHECK (BranchId like 'B%'),
-  BranchName varchar2(30) CONSTRAINT branch_name_notnull NOT NULL
+  BranchId        varchar(2)   CONSTRAINT branch_pk                        PRIMARY KEY
+                                CONSTRAINT branch_bid_check                 CHECK (BranchId like 'B%'),
+  BranchName      varchar(30)  CONSTRAINT branch_name_notnull              NOT NULL
 );
+
